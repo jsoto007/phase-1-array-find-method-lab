1cea87afac2aecb5732d973ca12bbfde2f0393a2
Jose Soto <jsoto7087@gmail.com>
2022-04-18 16:01:42 -0400

    commit tree 429f82e2f1124b2b1e673362a78140b2e0ed7852

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(record) {
+    let finder = record.find(obj => obj.result === 'W')
+    console.log(finder)
+    if (finder) {
+        return finder.year;
+    }
+}
 
+
+// will loop to array 
+// will find the first element that matches the description
